27428de957b65f275307ee0e6a19a6e2e4c07077
afanurkholik <137622326+afanurkholik@users.noreply.github.com>
2026-09-18 21:55:18 +0700

    Update next.config.js

--- portfolio-cakbiel/next.config.js
+++ portfolio-cakbiel/next.config.js
@@ -1,1 +1,13 @@
-
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,   // ← tambahkan ini
+  },
+  eslint: {
+    ignoreDuringBuilds: true,  // ← tambahkan ini
+  },
+  images: {
+    domains: ['placehold.co'],
+  },
+}
+module.exports = nextConfig
